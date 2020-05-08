6e36c526b569cb1e991c9ab5025b613cd5ed94ae
luisCermeno <luis.cermeno.f@gmail.com>
2020-05-08 15:00:54 -0700

    Exercise P2a Updated with feedback

--- Assigments/TERM PROJECT/Exercise P2b/email2.cpp
+++ Assigments/TERM PROJECT/Exercise P2b/email2.cpp
@@ -1,7 +1,7 @@
 //Objective:  template for C++ programs and to test code
 //Name: Luis Cermeno-Farro 1837034
 //Course: COMSC-110-8076
-//Compiler: Xcode
+//Compiler: Apple clang version 11.0.3
 //Editor: TextEdit
 
 //libraries
@@ -21,10 +21,9 @@ ifstream fin; //object for file input
 
 //Programmer defined functions
 void introduction(string obj);//user introduction
-bool validateInput(string inputName); //input validation function
-string getFileName(string type, string defaultName);//input function (for either input or output)
+string getFileName(string filetype, string dFileName);//input function (for either input or output)
 void readFile(string iFileName);//reads input file until EOF that calls function processLine for each line read from the input file
-
+int processLine(string lineFromFile);
 
 //main program
 int main()
@@ -70,28 +69,12 @@ void introduction(string obj)//user introduction
   cout << "Compiler(s) used: Xcode\n"; 
   cout << "File: " << __FILE__ << endl; 
   cout << "Complied: " << __DATE__ << " at " << __TIME__ << endl << endl;
-  cout << endl << "This program will prompt you for two text file names, an input and an output file. The program will read your input file and then show the lines containing the character '@'.Please type the name of each file correctly including the extension .txt. No other file extension is yet supported." << endl << endl;
+  cout << endl << "This program will prompt you for two text file names, an input and an output file. The program will read your input file and then show the lines containing the character '@'.Please filetype the name of each file correctly including the extension .txt. No other file extension is yet supported." << endl << endl;
 }//introduction function
 
 
-//input validation function
-bool validateInput(string inputName)
-{
-  //Data
-  bool isValid = false; // (boolean) is true when the input is valid
-  int n = inputName.length(); // (integer) is the number of characters of the text input
-  //Evaluation
-  if ( (n < 5) || (inputName[n-1] != 't' || inputName[n-2] != 'x' || inputName[n-3] != 't' || inputName[n-4] != '.') )
-  {
-    cout << "Sorry, that is an invalid name." << endl << "The file name must have the .txt extension. Try again please." << endl << endl;
-  }
-  else isValid = true;
-  return isValid;
-}//input validation function
-
-
 //input function (for either input or output)
-string getFileName(string type, string defaultName)
+string getFileName(string filetype, string dFileName)
 {
   //Data
   bool valid; // (boolean) is true when the input is already processed by validation function
@@ -99,19 +82,25 @@ string getFileName(string type, string defaultName)
   //User input
   while(true)
   {
-    if (type == "input")  cout << "Enter input filename [Press ENTER for default: " << defaultName << " ]: ";
-    else if (type == "output") cout << "Enter output filename [Press ENTER for default: " << defaultName << " ]: ";
+    if (filetype == "input")  cout << "Enter input filename [Press ENTER for default: " << dFileName << " ]: ";
+    else if (filetype == "output") cout << "Enter output filename [Press ENTER for default: " << dFileName << " ]: ";
     getline(cin,userInput);
     //Interpretation of ENTER key
     if (userInput.length() == 0)
     {
-      userInput = defaultName;
+      userInput = dFileName;
       break;
     }
     //Input Validation
     else
     {
-      valid = validateInput(userInput);
+      bool valid = false; // (boolean) is true when the input is valid
+      //Evaluation
+      if ( (userInput.length() < 5) || (userInput[userInput.length()-1] != 't' || userInput[userInput.length()-2] != 'x' || userInput[userInput.length()-3] != 't' || userInput[userInput.length()-4] != '.') )
+      {
+    cout << "Sorry, that is an invalid name." << endl << "The file name must have the .txt extension. Try again please." << endl << endl;
+      }
+      else valid = true;
       if (valid) break;
       else continue;
     }
@@ -124,22 +113,33 @@ string getFileName(string type, string defaultName)
 void readFile(string iFileName)
 {
   //Data
-  string readLine; //(text) is a line read from the input file
-  int i; //(integer) is a counter used to process the line read a look for '@'
+  string lineFromFile; //(text) is a line read from the input file
+  int nLines = 0; // (integer) number of lines of the file
+  int nATs; // (integer) number of '@' character found in each line, returned to readFile subprogram
+  int i; //(integer) is a counter used to process the line read a look for '@'s
+
   //Open and read the file
   fin.open(iFileName.c_str());
   if (!(fin.good())) throw "I/O error";
   while (fin.good())
     {
-      getline(fin, readLine);
-      //Process readLine to check for '@'
-      for (i = 0; i < readLine.length() ; i++)
-      {
-        if (readLine[i] == 64) cout << readLine << endl; //compare each character with ASCII64 which is '@'
-      }
+      getline(fin, lineFromFile);
+      //Process lineFromFile to check for '@'
+      nATs = processLine(lineFromFile);
+      for (i = 0; i < nATs; i++) cout << lineFromFile << endl;
+      nLines++;
     }//while fin.good
   fin.close();
 }//reads input file until EOF that calls function processLine for each line read from the input file
 
-
+int processLine(string lineFromFile)
+{
+  int i; //(integer) is a counter used to each character of the line
+  int nATs = 0; // (integer) number of '@' character found in each line, returned to readFile subprogram
+  for (i = 0; i < lineFromFile.length() ; i++)
+  {
+    if (lineFromFile[i] == 64) nATs++; //compare each character with ASCII64 which is '@'
+  }
+  return nATs;
+}
 
