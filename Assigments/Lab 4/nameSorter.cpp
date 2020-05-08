07b7c03815a15cb324a5f2c7e39eb492aa3db1c3
luisCermeno <luis.cermeno.f@gmail.com>
2020-05-08 16:27:08 -0700

    nameSorter.cpp done

--- Assigments/Lab 4/nameSorter.cpp
+++ Assigments/Lab 4/nameSorter.cpp
@@ -5,13 +5,25 @@
 //Editor: TextEdit
 
 //libraries
+#include <algorithm> //required for conversion to lowercase
 #include <fstream>
 #include <iostream>
 #include <string>
 using namespace std;
 
+#include <cctype> //required for conversion to lowercase
+
 //Programmer defined data types
 ifstream fin;
+
+class toLower //required for conversion to lowercase
+{
+  public: char operator() (char c) 
+  const 
+  {
+    return tolower(c);
+  }
+};
 
 //Special compiler dependent definitions
 //NONE
@@ -30,10 +42,12 @@ int main()
   //Data
   string objective = "Objective: Read names from a text file and sort them alphabetically\n";
   string inputFileName;
+  string lineFromFile;
   const int MAX_NAMES = 5; //list capacity
   int nNames = 0;// initially empty list
   string name[MAX_NAMES];// array-based list with a capacity of 5 integers
-  
+  int i;
+  int j;
 
   //user introduction
   introduction(objective);
@@ -42,16 +56,44 @@ int main()
   inputFileName = getFileName();
 
   //store the names
+  bool noDuplicate = true;
   fin.open(inputFileName.c_str());
   if (!(fin.good())) throw "I/O error";
   while (fin.good() && nNames < 5)
   {
-    getline(fin, name[nNames]);
+    getline(fin, lineFromFile);
+    if (lineFromFile.length() > 0)  //to skip blank spaces
+    {
+      for (i = 0; i < nNames; i ++) //to check for duplicates
+      {
+        string lc_lineFromFile = lineFromFile;
+        string lc_name = name [i];
+        transform (lc_lineFromFile.begin(), lc_lineFromFile.end() , lc_lineFromFile.begin(), toLower());
+        transform (lc_name.begin(), lc_name.end() , lc_name.begin(), toLower());
+        if (lc_lineFromFile == lc_name) noDuplicate = false;
+      }
+      if (noDuplicate) name[nNames] = lineFromFile;
+    }
     nNames++;
   }//while fin.good
 
-
-
+  //sort loops
+  string temp;
+  for (i = 0; i < (nNames - 1); i++)
+  {
+    for (j = i + 1; j < nNames; j++)
+    {
+      if (toupper(name[i][0]) > toupper(name[j][0]))
+      {
+        temp = name[i];
+        name[i] = name[j];
+        name[j] = temp;
+      } //if
+    } // inner j for loop
+  } // outter i for loope
+  
+  //output names sorted
+  for (i = 0; i < nNames; i ++) cout << name[i] << endl;
 }//main
 
 
